a9dfd670a3c08e982b00d5ee6e424629a212fbe8
seongho <seongho.com@gmail.com>
2013-08-20 20:55:49 +0900

    seongho lee commit

---
+++ mission07/basic/Seongho.Lee/blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '7e11d6fe279c7d9614c051e1d3e799ee5232fa69cafa148bd019dd401310de8e12995972907146916cf7e3ca5b59afd60c926aa9f8384f69b0a60fece91d98e0'
